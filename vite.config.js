bca3d92af64a814fa003cf144d3b5370b55c34d8
Jimmy DiColandrea <jdicolandrea@gmail.com>
2024-06-12 22:07:05 -0400

    allow Vite to refesh on page save

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ViteRestart from 'vite-plugin-restart'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+        usePolling: true
+    }
+}
 })
+ 
