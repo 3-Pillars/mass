7813c949fac4c913170d69d9916820c8ff8d6c19
3-Pillars <hdandtheboyz@gmail.com>
2024-07-25 19:17:40 -0400

    change base in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import ViteRestart from 'vite-plugin-restart'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mass/',
+  base: '/',
   plugins: [react()],
   server: {
     watch: {
