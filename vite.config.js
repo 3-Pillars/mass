8c360406fe516346ac861fa19573712c28103d69
3-Pillars <hdandtheboyz@gmail.com>
2024-07-28 21:44:53 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import ViteRestart from 'vite-plugin-restart'
 
 export default defineConfig({
-  base: '/',
+  base: '/mass/',
   plugins: [react()],
   server: {
     watch: {
