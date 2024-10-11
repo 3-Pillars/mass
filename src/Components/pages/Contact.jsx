3a97c956759c574a9c97fc25099900f464651e7c
3-Pillars <hdandtheboyz@gmail.com>
2024-10-10 23:42:32 -0400

    basic useless contact from

--- src/Components/pages/Contact.jsx
+++ src/Components/pages/Contact.jsx
@@ -1,8 +1,42 @@
 'use strict'
+import '../../../public/contact.css'
 
 function Contact() {
+
+  function search(formData) {
+    const query = formData.get("query");
+    alert(`You searched for '${query}'`);
+  }
+
   return (
-    <></>
+    <>
+    <div id="contact-container">
+      <h1 id="header-text">Ready to make a change?</h1>
+      <h1>We can help!</h1>
+      <form action={search}>
+        <div>
+          <label>Name: </label>
+        </div>
+        <div>
+          <input name="query" />
+        </div>
+        <div>
+          <label>Email: </label>
+        </div>
+        <div>
+          <input name="query" />
+        </div>
+        <div>
+          <label>Briefly tell us how we can help you</label>
+        </div>
+        <div>
+          <textarea></textarea>
+        </div>
+        <br />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+    </>
   );
 }
 
