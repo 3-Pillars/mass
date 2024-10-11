fd4602eee46a3919b3980b2e4e66d2ca714afa98
3-Pillars <hdandtheboyz@gmail.com>
2024-10-11 00:54:08 -0400

    add state and more basic styling

--- src/Components/pages/Contact.jsx
+++ src/Components/pages/Contact.jsx
@@ -1,5 +1,7 @@
 'use strict'
+import React, {useState} from 'react';
 import '../../../public/contact.css'
+
 
 function Contact() {
 
@@ -8,33 +10,48 @@ function Contact() {
     alert(`You searched for '${query}'`);
   }
 
+  const [submitted, changeSubmitted] = useState(false)
+
   return (
     <>
     <div id="contact-container">
       <h1 id="header-text">Ready to make a change?</h1>
       <h1>We can help!</h1>
       <form action={search}>
-        <div>
-          <label>Name: </label>
+
+        <div className='form-section'>
+          <div className='form-element'>
+            <label>Name: </label>
+          </div>
+          <div className='form-element'>
+            <input name="query" />
+          </div>
         </div>
-        <div>
-          <input name="query" />
+
+        <div className='form-section'>
+          <div className='form-element'>
+            <label>Email: </label>
+          </div>
+          <div className='form-element'>
+            <input name="query" />
+          </div>
         </div>
-        <div>
-          <label>Email: </label>
+        
+        <div className='form-section'>  
+          <div className='form-element'>
+            <label>Briefly tell us how we can help you</label>
+          </div>
+          <div className='form-element'>
+            <textarea></textarea>
+          </div>
         </div>
-        <div>
-          <input name="query" />
+        
+        
+        <div id="submit-button" onClick={() => changeSubmitted(true)}>
+          <div>Begin your</div><div>journey</div>
         </div>
-        <div>
-          <label>Briefly tell us how we can help you</label>
-        </div>
-        <div>
-          <textarea></textarea>
-        </div>
-        <br />
-        <button type="submit">Search</button>
       </form>
+      {submitted ? <h1>Submitted!</h1> : null}
     </div>
     </>
   );
