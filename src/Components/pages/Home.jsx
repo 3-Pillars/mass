ce263a92d88b2381d850f762a1da79015fa96137
3-Pillars <hdandtheboyz@gmail.com>
2025-02-21 15:12:10 -0500

    update slideshow and homepage styling

--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
   return (
     <>
       <Carousel />
-      <div className='page-body'>
+      <div className='page-body' id="homepage">
         <h1>OUR MISSION</h1>
         <span>There is unseen potential in the world around us everyday which goes untapped and unrealized. In this divided age of information overload we need to be selective about our attention and experiences.  Without focus life can feel out of control or out of focus.  At Mass© the best version of you is our focus.  Our system provides the path to Great Restructuring of one's life, which will help you Navigate© to the next level.  This will allow for each individual to feel connected and present; and ultimately lead to the ultimate convergence with the world around you.</span>
         <span>If all beings are meant for something, why try to improve, compete, achieve, or thrive? Navigating to your end goals in the future means converging with your destiny in the present.  If we are all predestined then you can realize your goals in the present to restructure your reality and affect lasting change today.  If we aren’t predestined, envisioning and acting upon your goals is still the best pathway to success tomorrow!</span>
