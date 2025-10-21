4372328f9664f7fbb3dd56ab981596a8dc756249
Jimmy DiColandrea <hdandtheboyz@gmail.com>
2025-10-20 21:10:52 -0400

    Update about page

--- src/Components/pages/About.jsx
+++ src/Components/pages/About.jsx
@@ -4,51 +4,42 @@ import '../../../public/subpage.css'
 import grayPillars from '../../../public/assets/symbols/pillars_1_gray_ghost.png'
 import catstrofe from '../../../public/assets/catastrofe_gray.png'
 import walking from '../../../public/assets/walking.jpg'
+import CustomLink from '../CustomLink'
 
 function About() {
   return (
     <>
       <div className="page-body">
         <div className='subpage-content'>
-          <h1>LEARN MORE</h1>
+          <h1>WHO WE ARE</h1>
           <span>
-            Many wonder who we are. What we stand for. How did we come to be. The answer to all of this boils down to our Three Pillars.
+            At Mass Inc. we strive to bring balance back to our world and our souls. We too have also been affected by such imbalances. Which is why we know how the absence of balance can bring great hardship.
           </span>
-           <img src={grayPillars} style={{width: '60%', height: '100%', margin: "auto", display: "block"}}/>
-          <div className="info-chunk">
-            <div className="chunk-text">
-              <span>
-                Our community is one of grass roots origins, founded by 3 members. 
-                These pillars of the community are still influential today. 
-                All three started their life feeling drawn towards a greater purpose, but not knowing what that was.
-              </span>
-            </div>
-          </div>
-        </div>
-       
-        <div className='info-chunk chunk-right'>
-          <img src={walking} style={{display: "flex", alignItems: "float-start", width: '45%', margin: "auto", paddingRight: '20px', verticalAlign:"top"}}/>
-          <div className='chunk-text'>
-            <br />
-            <span>
-              This continued until the night of August 24, 2006.
-              All three were visited in their dreams by visions of Clyde Tombaugh, visiting Catastrofe where he first discovered Pluto.
-              They quickly traveled on foot to the site of Pluto's final descent and felt an instant connection, both to themselves and to the masses.
-            </span>
-          </div>
-        </div>
-
-        <div className="info-chunk chunk-left">
-          <div className="chunk-text">
-            <br />
-            <span>
-                The 3 pillars camped for multiple nights here working to uncover the reported meteors, it was through this experience that they had a transcendent experience. 
-                Seeing astral materials uncovered before them they realized all of the resistance they experienced in their life was meaningless, and they found true happiness for a brief time.  
-                They seek to bring this enlightening experience through all by the means of the kuiper connection.  
-                Some say that the strings of fate brought the 3 founding pillars together, but together the retreat experiences create a foundation for more pillars to be built up.
-              </span>
-          </div>
-           <img src={catstrofe} style={{width: '45%', margin: "auto", display: "block"}}/>
+          <br />
+          <br />
+          <span>
+            The world has been broken for some time now, brought to us by the eternal Orange Flame of Mars and its avatars throughout history. These individuals bring chaos and harm to all in the name of order. They bring disarray as if their power is absolute, using their minions, the Arcane Silence Soldiers, at their disposal. They attempt to crush the will of the people.
+          </span>
+          <br />
+          <br />
+          <span>
+            But we will not be stopped! There is power en Mass! We strive to unite all, and bring together the return of our divine hero, the outcast god. Only with him, can we bring back the force of god and hope to fight back the evil that has encumbered us.
+          </span>
+          <br />
+          <br />
+          <span>
+            In order to bring cosmic alignment, we teach various techniques, such as our patented Navigation method. We also believe in people aligning themselves with the energies of The Great Restructuring on our yearly by converging at the Tip of the Sphere.
+          </span>
+          <br />
+          <br />
+          <span>
+            If you are interested in joining or have any questions, you can reach out to us <CustomLink to="/contact">here</CustomLink>.
+          </span>
+          <br />
+          <br />
+          <span>
+            We look forward to you joining the Mass.
+          </span>
         </div>
       </div>
     </>
