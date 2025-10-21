003414fe2ce2676c7b5f59863dd1003a23c09215
Jimmy DiColandrea <hdandtheboyz@gmail.com>
2025-10-20 20:56:23 -0400

    move about to history page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './Components/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Components/pages/Home'
 import About from './Components/pages/About'
+import History from './Components/pages/History'
 import Techniques from './Components/pages/Techniques'
 import Retreats from './Components/pages/Retreats'
 import Contact from './Components/pages/Contact'
@@ -21,6 +22,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
+          <Route path="/history" element={<History />}></Route>
           <Route path="/techniques" element={<Techniques />}></Route>
           <Route path="/retreats" element={<Retreats />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
